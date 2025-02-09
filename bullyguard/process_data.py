5fa4240b61d48967c39696672513efbdcedc77c7
BarG3126 <bar3126@gmail.com>
2025-02-09 19:02:15 +0200

    Initial working version of getting raw data

--- bullyguard/process_data.py
+++ bullyguard/process_data.py
@@ -1,14 +1,30 @@
 from bullyguard.config_schemas.config_schema import Config
 from bullyguard.utils.config_utils import get_config
 from bullyguard.utils.gcp_utils import access_secret_version
+from bullyguard.utils.data_utils import get_raw_data_with_version
 
 
 @get_config(config_path="../configs", config_name="config")
 def process_data(config: Config) -> None:
-    print(config)
-
+    version = "v1"
+    data_local_save_dir = "./data/raw"
+    dvc_remote_repo = "https://github.com/BarG3126/bullyguard-data.git"
+    dvc_data_folder = "data/raw"
+    github_user_name = "BarG3126"
     github_access_token = access_secret_version("ml-project-447013", "bullyguard-data-github-access-token")
-    print(f"{github_access_token=}")
+
+    try:
+        get_raw_data_with_version(
+            version=version,
+            data_local_save_dir=data_local_save_dir,
+            dvc_remote_repo=dvc_remote_repo,
+            dvc_data_folder=dvc_data_folder,
+            github_user_name=github_user_name,
+            github_access_token=github_access_token,
+        )
+    except Exception as e:
+        print(f"Error occurred: {str(e)}")
+        # Add more detailed error logging here if needed
 
 
 if __name__ == "__main__":
---
+++ bullyguard/utils/data_utils.py
@@ -0,0 +1,35 @@
+import subprocess
+from shutil import rmtree
+
+def get_cmd_to_get_raw_data(
+    version: str,
+    data_local_save_dir: str,
+    dvc_remote_repo: str,
+    dvc_data_folder: str,
+    github_user_name: str,
+    github_access_token: str,
+) -> str:
+    without_https = dvc_remote_repo.replace("https://", "")
+    dvc_remote_repo = f"https://{github_user_name}:{github_access_token}@{without_https}"
+    command = f"dvc get {dvc_remote_repo} {dvc_data_folder} --rev {version} -o {data_local_save_dir}"
+    return command
+
+def get_raw_data_with_version(
+    version: str,
+    data_local_save_dir: str,
+    dvc_remote_repo: str,
+    dvc_data_folder: str,
+    github_user_name: str,
+    github_access_token: str,
+) -> None:
+    rmtree(data_local_save_dir, ignore_errors=True)
+    command = get_cmd_to_get_raw_data(
+        version, data_local_save_dir, dvc_remote_repo, dvc_data_folder, github_user_name, github_access_token
+    )
+    try:
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        print(f"Command output: {result.stdout}")
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with exit code {e.returncode}")
+        print(f"Error output: {e.stderr}")
+        raise
